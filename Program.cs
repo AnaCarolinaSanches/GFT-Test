e8a6d73a76f7139747e7869a7041eeacf2777edd
Ana Carolina Sanches <70352645+AnaCarolinaSanches@users.noreply.github.com>
2021-02-11 17:55:12 -0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -26,11 +26,24 @@ namespace ConsoleApp1
             Console.WriteLine("Multiplicando: " + multipliciador);
             Console.WriteLine("Inicio: " + inicio);
             Console.WriteLine("Fim: " + fim);
-
-            for (int i = 0; i <= fim; i++)
+            
+            if (multipliciador > 3000 || multipliciador < 0)
+                Console.WriteLine("Multiplicador não pode ser negativo e maior que 3000");
+            else if (inicio > 3000 || inicio < 0 || fim > 3000 || fim < 0)
+                Console.WriteLine("O Intervalo não pode conter negativos e nem ser maior que 3000");
+            else if ((inicio - fim) > 10)
+                Console.WriteLine("O intervalo não pode ser maior que 10");
+            else if (inicio >= fim)
+                Console.WriteLine("inicio deve ser menor que o fim");
+            else
             {
-                Console.WriteLine(multipliciador + " x " + i + " = " + (i * multipliciador));
+
+                for (int i = 0; i <= fim; i++)
+                {
+                    Console.WriteLine(multipliciador + " x " + i + " = " + (i * multipliciador));
+                }
             }
+
         }
     }
 }
