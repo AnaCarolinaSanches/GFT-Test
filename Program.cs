25edc4943cda19846b85a9620403ee54d334dc92
ANA CAROLINA SANCHES <ana.sanches32@etec.sp.gov.br>
2021-02-11 16:43:55 -0300

    Teste GFT

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int multipliciador = 0;
+            int inicio = 0;
+            int fim = 0;
+
+            Console.WriteLine("Digite o multiplicador: ");
+            multipliciador = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o Inicio do intervalo");
+            inicio = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Digite o fim do intervalo");
+            fim = int.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Multiplicando: " + multipliciador);
+            Console.WriteLine("Inicio: " + inicio);
+            Console.WriteLine("Fim: " + fim);
+
+            for (int i = 0; i <= fim; i++)
+            {
+                Console.WriteLine(multipliciador + " x " + i + " = " + (i * multipliciador));
+            }
+        }
+    }
+}
